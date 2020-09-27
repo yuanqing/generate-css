5b1e0d33a8a4f13d666866b305a6e6c9fe2e5103
yuanqing <desk@yuanqing.sg>
2020-09-27 11:47:14 +0800

    Add tests for `parse-class-name`

---
+++ src/utilities/create-css/__tests__/parse-class-name.ts
@@ -0,0 +1,83 @@
+import { test } from 'tap'
+
+import { parseClassName } from '../parse-class-name'
+
+test('empty string', function (t) {
+  t.plan(1)
+  t.throws(function () {
+    parseClassName('', [])
+  })
+})
+
+test('plain selector', function (t) {
+  t.plan(1)
+  t.deepEqual(parseClassName('block', []), {
+    breakpoint: null,
+    pseudoClass: null,
+    selector: 'block'
+  })
+})
+
+test('invalid media query', function (t) {
+  t.plan(1)
+  t.throws(function () {
+    parseClassName('sm@block', [])
+  })
+})
+
+test('valid media query', function (t) {
+  t.plan(1)
+  t.deepEqual(parseClassName('sm@block', ['sm']), {
+    breakpoint: 'sm',
+    pseudoClass: null,
+    selector: 'block'
+  })
+})
+
+test('pseudo-class', function (t) {
+  t.plan(1)
+  t.deepEqual(parseClassName('block:hover', []), {
+    breakpoint: null,
+    pseudoClass: {
+      isGroup: false,
+      value: 'hover'
+    },
+    selector: 'block'
+  })
+})
+
+test('group pseudo-class', function (t) {
+  t.plan(1)
+  t.deepEqual(parseClassName('block:group-hover', []), {
+    breakpoint: null,
+    pseudoClass: {
+      isGroup: true,
+      value: 'hover'
+    },
+    selector: 'block'
+  })
+})
+
+test('valid media query and pseudo-class', function (t) {
+  t.plan(1)
+  t.deepEqual(parseClassName('sm@block:hover', ['sm']), {
+    breakpoint: 'sm',
+    pseudoClass: {
+      isGroup: false,
+      value: 'hover'
+    },
+    selector: 'block'
+  })
+})
+
+test('valid media query and group pseudo-class', function (t) {
+  t.plan(1)
+  t.deepEqual(parseClassName('sm@block:group-hover', ['sm']), {
+    breakpoint: 'sm',
+    pseudoClass: {
+      isGroup: true,
+      value: 'hover'
+    },
+    selector: 'block'
+  })
+})
--- src/utilities/create-css/parse-class-name.ts
+++ src/utilities/create-css/parse-class-name.ts
@@ -6,6 +6,9 @@ export function parseClassName(
   className: string,
   breakpoints: Array<string>
 ): ParsedClassName {
+  if (className === '') {
+    throw new Error('Class name is an empty string')
+  }
   const matches = className.match(classRegex)
   if (matches === null) {
     return {
@@ -14,12 +17,12 @@ export function parseClassName(
       selector: className
     }
   }
-  const breakpoint =
-    typeof matches[1] === 'undefined' || breakpoints.indexOf(matches[1]) === -1
-      ? null
-      : matches[1]
+  const breakpoint = typeof matches[1] === 'undefined' ? null : matches[1]
+  if (breakpoint !== null && breakpoints.indexOf(breakpoint) === -1) {
+    throw new Error(`Invalid breakpoint: ${breakpoint}`)
+  }
   return {
-    breakpoint: breakpoint,
+    breakpoint,
     pseudoClass:
       typeof matches[3] === 'undefined' ? null : parsePseudoClass(matches[3]),
     selector: matches[2]
