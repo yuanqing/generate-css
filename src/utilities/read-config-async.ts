1b3dbabc4e3513d336d2658ec837232f109123c8
yuanqing <desk@yuanqing.sg>
2020-10-04 23:07:13 +0800

    Add default values

--- src/utilities/read-config-async.ts
+++ src/utilities/read-config-async.ts
@@ -3,10 +3,15 @@ import * as fs from 'fs-extra'
 import { CliOptions, Config } from '../types'
 
 export async function readConfigAsync(cliOptions: CliOptions): Promise<Config> {
+  if ((await fs.pathExists(cliOptions.configFilePath)) === false) {
+    throw new Error(`File not found: ${cliOptions.configFilePath}`)
+  }
   const config = JSON.parse(
     await fs.readFile(cliOptions.configFilePath, 'utf8')
   )
   return {
+    reset: true,
+    theme: {},
     ...cliOptions,
     ...config
   }
